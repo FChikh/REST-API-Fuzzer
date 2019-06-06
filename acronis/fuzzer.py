813d315e910609a9640f4773d652575903b85338
FChikh <fchikhachev@gmail.com>
2019-06-06 15:15:37 +0300

    small bug

--- acronis/fuzzer.py
+++ acronis/fuzzer.py
@@ -3,7 +3,7 @@ import json
 import random
 import string
 os.system('node parser.js')
-with open('parsed.json') as json_file:
+with open('parsed.json', 'r') as json_file:
     data = json.load(json_file)
 print(data.keys())
 data = {'types': data['types'], 'resources': data['resources'], 'baseUri': data['baseUri']}
