48f96206143ac14399980f083d975349fcc92e6f
Дегтеренко Егор <egor.larin.2015@mail.ru>
2019-06-06 12:53:59 +0300

    at least it is commit not Megatron

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # REST-API-Fuzzer
+
+parser.js - parser on js, used in fuzzer.py, to change .raml file you need to change 7th line in parser.js
+fuzzer.py - fuzzer on python
+ 
--- acronis/parser.js
+++ acronis/parser.js
@@ -1,10 +1,43 @@
 console.log('RAML 1.0 JS Parser Test');
 
-var fs = require('fs');
 var path = require("path");
-var raml1Parser = require('../dist/index');
+var raml1Parser = require('../js-parser/dist/index');
+var fs = require("fs")
 
-var api = raml1Parser.loadApiSync(path.resolve(__dirname, "./api.raml"));
+var api = raml1Parser.loadApiSync(path.resolve(__dirname, "./api.raml"),{
+
+	fsResolver: {
+
+		content: function(path){ return fs.readFileSync(path).toString(); },
+
+		list: function(path){ return fs.readDirSync(path); },
+
+		contentAsync: function(path){
+			return new Promise(function (resolve, reject) {
+	            fs.readFile(path, function (err, data) {
+	                if (err != null) {
+	                    reject(err);
+	                    return;
+	                }
+	                var content = data.toString();
+	                resolve(content);
+	            });
+          	});
+		},
+
+		listAsync: function (path) {
+	        return new Promise(function (reject, resolve) {
+	            fs.readdir(path, function (err, files) {
+	                if (err != null) {
+	                    reject(err);
+	                    return;
+	                }
+	                resolve(files);
+	            });
+	        });
+	    }
+	}
+});
 
 api.errors().forEach(function(x){
     console.log(JSON.stringify({
